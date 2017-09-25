6f1e199a1871a72a1a1c22ea74364c59e86ce186
yulindaramadhana <yulinda.ramadhana98@gmail.com>
2017-09-25 07:51:32 +0700

    push2

--- portfolio5.php
+++ portfolio5.php
@@ -1,190 +1,286 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title>Cosmics Group</title>
-  <meta http-equiv="X-UA-Compatible" content='IE=edge'>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-<!--   <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'> -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.4.0/animate.min.css">
-<style type="text/css">
-   body,html {
-  height:100%;
-  margin:0;
-  background:black;
-  font-family: sans-serif!important;
-  font-size: 16px;
-  }
-
-#Home {
-  background: url(gambar1.jpg);
-  background-size: cover;
-  background-position: center center;
-  background-attachment: fixed;
-  height: 100%;
-  color: black;
-  text-align: center;
-  padding: auto;
-  padding-top: 10%;
-  position: relative;
-}
-
-#AboutGroup {
-  background: url(gambar3.jpg);
-  background-size:cover;
-  background-position:center center;
-  background-attachment:fixed;
-
-}
-
-/*logo*/
-#logo {
-  height:50px; width:60px; 
-  float:left; margin:-15px 25px 0 -15px; 
-  }
-
-#logo:hover {
-  background-color: rgba(255, 255, 255, 0.05); 
-  border-radius: 10px
-  }
-
-/*nav menu */
-#Hd {
-  margin: 100px 100px 50px 100px;
-  padding: 50px;
-  }
-
-.Hd {
-  color:#fff; background-color: rgba(0, 0, 0, 0.7);
-  border-radius: 5px;
-  }
-
-.btn {
-  white-space: normal;
-  word-break: normal;
-}
-
-#mainMenu a:hover {
-  background-color: #5CB85C; 
-  border-radius: 5px}
-
-.dropdown-menu a:hover {
-  background-color: rgba(0, 0, 0, 0.2)!important; 
-  border-radius: 0!important;
-  }
-
-#buttons button {margin: 2px;}
-.frosted {
-  background-color: rgba(256,256,256,0.8);
-  width: 75%;
-  height: auto;
-  margin: auto;
-  border-radius: 15px;
-  margin-top: 30px;
-  padding: 20px;
-  overflow: auto;
-}
-
-.xy {
-  margin-top: 15px;
-}
-.xy > a {
-  font-weight: 700;
-  padding: 5px 25px;
-}
-
-.rd {
-  width: 40%;
-  float: right;
-  display: inline;
-  height: 100%;
-  line-height: 400px;
-}
-
-
-
-/* Medium Devices, Desktops /-md-/*/
-@media only screen and (max-width : 992px) {
-.jumbotron {margin: 0  auto;}
-#rowPortf { margin:0 auto;}
-}
-
-/* Small Devices, Tablets /-sm-/*/
-@media only screen and (max-width : 768px) {
-.img-zoom-out {margin: 40px!important}
-#Hd {margin: 50px 0 50px 0; padding: 0 0 50px 0;}
-#rowPortf { margin:0 0px  0 auto; overflow: hidden}
-}
-
-/* Extra Small Devices, Phones /-xs-/ */
-@media only screen and (max-width : 480px) {
-#my img {margin: 30px 70px 30px 90px;}
-.jumbotron {padding: 20px 0 20px 0;}
-.desktop, .mobile {display:none;}
-.tablet {top:0; left:8%}
-.carousel-indicators {left:27%!important;}
-.carousel-indicators>li, .carousel-indicators>.active {
-  width:2em!important; height:2em!important; border-radius: 50%!important;
-}
-.devices {height:422px;}
-#rowPortf { margin:0 auto;}
-#rowPortf h2{margin-left: -50px}
-}
-
-/* Custom, iPhone Retina */
-@media only screen and (max-width : 320px) {
-.tablet {top:0; left:5%}
-.carousel-indicators {left:24%;}
-#rowPortf h2{margin-left: -70px}
-}
-
-</style>
-</head>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cosmics Group</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.min.css">
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  </head>
 <body>
-	<!-- Navbar-->
 
-<div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-header">
-      <a href="#Hd" class="navbar-brand"><img src="#" height="50" width="64" id="logo" alt="logo">Cosmics Group</a>
-      </div>
-      <div id="mainMenu" class="collapse navbar-collapse">
-        <ul class="nav navbar-nav">
-          <li><a href="#Home">Home</a></li>
-          <li><a href="#AboutGroup">About Group</a></li>
-          <li><a href="#" data-toggle="modal" data-target='#modal'>Contacts</a></li>
-          <li class='dropdown'>
-            <a href="#" class='dropdown-toggle' data-toggle='dropdown'>Portfolio </li>
-              </ul>
-           </li>
-         </ul>
-      </div>
-    </div>
-    <!-- Navbar-->
-   <section id="Home">
-   		<div class="jumbotron frosted">
+<div class="navbar navbar-default navbar-fixed-top" role="navigation" id="myNavbar">
+	<div class="container">
+		<div class="nabar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button> 
+
+			<a href="#" class="navbar-brand">COSMIC</a>
+		</div>
+		<div class="navbar-collapse collapse">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#Home">Home</a></li>
+				<li><a href="#AboutGroup">About Group</a></li>
+				<li><a href="#recent_work">Product</a></li>
+				<li><a href="#team">Tugas Besar</a></li>
+				<li><a href="#" data-toggle="modal" data-target='#modal'>Contact</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<!--Home-->
+	<section id="Home" class="header">
+   		<div class="jumbotron frosted wow bounceUp">
    			<h1>Web Design & Development</h1>
    			<h2>We are Cosmic Group</h2>
    		</div>
-   		<div class="xy">
-   			<a type="button" class="btn btn-default" href="#" target="_blank">Facebook</a>
-   			<a type="button" class="btn btn-default" href="#" target="_blank">Github</a>
-   			<a type="button" class="btn btn-default" href="#" target="_blank">Twitter</a>
-   		</div>
+   			<button class="btn btn-lg btn-primary wow bounceInUp">Join Now</button>
+   			<button class="btn btn-lg btn-success wow bounceInUp">View Detail</button>
+   		
    </section>
+<!--Home-->
 
-   <section id="AboutGroup">
-   		<div class="jumbotron frosted">
-   			<div class="Id">
-   				<h2>Andrea Kulbaba</h2>
-        <p>A pastry chef by trade, I decided to learn a new skill: web development!</p>
-        <p>Based in the north of England, I try to get involved with the web development community when my schedule allows it.</p>
-        <p>I'm currently expanding my skillset by diving more in depth with JavaScript.</p>
-   			</div>
-   			<div class="rd">
-          <img id="biopic" src="#"/>
+<!--About-->
+   <section id="AboutGroup" class="AboutGroup">
+     	<div class="container" id="container1">
+          <div class="row">
+            <div class="col-lg-12 page-header text-center">
+              <h2><b>MEET OUR TEAM</b></h2>
+            </div>
+          </div>
         </div>
-   		</div>
+
+        <div class="container" id="container2" >
+          <div class="row">
+            <div class="col-lg-3 col-sm-12 text-center wow fadeInLeft" data-wow-delay="2s"> <img class="img-circle" alt="140x140" style="width: 140px; height: 140px;" src="gambar1.jpg" data-holder-rendered="true">
+              <h3><b>Elin Darnela</b></h3>
+              <p>Lahir di Prabumulih, 13 Juni 1997<br>
+                 Motto hidup "Lakukan yang engkau bisa agar engkau menjadi Suka,<br>
+                 yang berdiam akan tertinggal maka Bergeraklah"
+              </p>
+              <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+              <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+              <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+            </div>
+
+            <div class="col-lg-3 col-sm-12 text-center wow fadeInLeft" data-wow-delay="1.6s"><img class="img-circle" alt="140x140" style="width: 140px; height: 140px;" src="gambar1.jpg" data-holder-rendered="true">
+              <h3><b>Pipit Kurnia Sari</b></h3>
+              <p>Lahir di Mekarsari, 28 Agustus 1998<br>
+                 Motto hidup "Percaya, Ada rencana terbaik"
+              </p>
+              <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+              <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+              <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+            </div>
+
+            <div class="col-lg-3 col-sm-12 text-center wow fadeInLeft" data-wow-delay="1.2s"><img class="img-circle" alt="140x140" style="width: 140px; height: 140px;" src="gambar1.jpg" data-holder-rendered="true">
+              <h3><b>Lifya Fitriani</b></h3>
+              <p>Lahir di Palembang, 12 febuari 1998<br>
+                 Motto hidup "You will never be brave if you don't get hurt<br>
+                 You will never learn if you don't make a mistake<br>
+                 You will never be successful if you don't counter failure<br>
+                 Informatics Engineering, Sriwijaya University '15"
+              </p>
+              <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+              <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+              <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+            </div>
+
+            <div class="col-lg-3 col-sm-12 text-center wow fadeInLeft" data-wow-delay="0.8s"><img class="img-circle" alt="140x140" style="width: 140px; height: 140px;" src="gambar1.jpg" data-holder-rendered="true">
+              <h3><b>Yulinda Ramadhana</b></h3>
+              <p>Lahir di Palembang, 10 Januari 1998<br>
+                 Motto hidup "Suffer today,<br>
+                 Enjoy tommorrow :)"
+              </p>
+              <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+              <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+              <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+            </div>
+          </div>        
+      </div>
    </section>
+<!--About--> 
+
+
+<!--Recent Work-->
+     <section id="recent_work">
+     	<div class="container" id="container1">
+          <div class="row">
+            <div class="col-lg-12 page-header text-center">
+              <h2><b>SOME OF OUR WORK</b></h2>
+            </div>
+          </div>
+        </div>
+
+        <div class="container" id="Portf">
+            <div class="row Hd" id="rowPortf">
+                <div class="col-lg-12 text-center">
+                      <!--carousel-->
+                      <div id="carousel" class="carousel slide">
+                      <!-- carousel indicators-->
+                          <ol class="carousel-indicators">
+                            <li class='active' data-target="#carousel" data-slide-to="0"></li>
+                            <li data-target="#carousel" data-slide-to="1"></li>
+                            <li data-target="#carousel" data-slide-to="2"></li>
+                            <li data-target="#carousel" data-slide-to="3"></li>
+                            <li data-target="#carousel" data-slide-to="4"></li>
+                          </ol>
+                      <div class="carousel-inner">
+                        <!--item active-->
+                             <div class="devices item active">
+                              <div class="desktop">
+                                  <a href="#" target="_blank">
+                                     <img src="gambar5.jpg"   alt="Calculator">
+                                 </a>
+                              </div>
+                              <div class="tablet">
+                                 <a href="#" target="_blank">
+                                     <img src="gambar5.jpg"   alt="Calculator">
+                                 </a>
+                              </div>
+                              <div class="mobile">
+                                 <a href="#" target="_blank">
+                                     <img src="gambar5.jpg"   alt="Calculator">
+                                 </a>
+                              </div>
+                            </div>
+                          <!--item active-->
+                          <!--item2-->
+                          <div class="devices item">
+                              <div class="desktop">
+                                    <a href="http://oky.16mb.com/MYpublic/yellow/" target="_blank">
+                                      <img src="http://oky.16mb.com/MYpublic/yellow.jpg"  alt="yellow">
+                                   </a>
+                                </div>
+                                <div class="tablet">
+                                   <a href="http://oky.16mb.com/MYpublic/yellow/" target="_blank">
+                                     <img src="http://oky.16mb.com/MYpublic/yellow.jpg"  alt="yellow">
+                                  </a>
+                                </div>
+                                <div class="mobile">
+                                   <a href="http://oky.16mb.com/MYpublic/yellow/" target="_blank">
+                                      <img src="http://oky.16mb.com/MYpublic/yellow.jpg"  alt="yellow">
+                                  </a>
+                                </div>
+                              </div>
+                            <!--item2-->
+                            <!--item3-->
+                            <div class="devices item">
+                              <div class="desktop">
+                                  <a href="https://codepen.io/oky/full/RroLzJ/" target="_blank">
+                                      <img src="http://oky.16mb.com/MYpublic/Web/Pomodoro_Clock.jpg" alt="shi">
+                                  </a>
+                              </div>
+                              <div class="tablet">
+                                  <a href="https://codepen.io/oky/full/RroLzJ/" target="_blank">
+                                    <img src="http://oky.16mb.com/MYpublic/Web/Pomodoro_Clock.jpg" alt="shi">
+                                  </a>
+                              </div>
+                              <div class="mobile">
+                                 <a href="https://codepen.io/oky/full/RroLzJ/" target="_blank">
+                                    <img src="http://oky.16mb.com/MYpublic/Web/Pomodoro_Clock.jpg" alt="shi">
+                                  </a>
+                              </div>
+                            </div>
+                            <!--item3-->
+                            <!--item4-->
+                             <div class="devices item">
+                              <div class="desktop">
+                                  <a href="#" target="_blank">
+                                     <img src="gambar3.jpg"   alt="Calculator">
+                                 </a>
+                              </div>
+                              <div class="tablet">
+                                 <a href="#" target="_blank">
+                                     <img src="gambar1.jpg"   alt="Calculator">
+                                 </a>
+                              </div>
+                              <div class="mobile">
+                                 <a href="#" target="_blank">
+                                     <img src="gambar2.jpg"   alt="Calculator">
+                                 </a>
+                              </div>
+                            </div>
+                        <!--item4-->
+                        
+                       </div><!--carousel inner-->
+                       <!--control-->
+                       <a href="#carousel" class="carousel-control left" data-slide="prev">
+                        <span class="gluphicon gluphicon-chevron-left"></span></a>
+                        <a href="#carousel" class="carousel-control right" data-slide="next">
+                        <span class="gluphicon gluphicon-chevron-right"></span></a>
+                  </div>
+            </div>
+         </div>
+    </section>
+
+<!--contact-->
+	<div id="modal" class="modal fade">
+	  <div class="modal-dialog">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	          <button data-dismiss="modal" class='pull-right'><i class="fa fa-close"></i></button>
+	           <h4 class='modal-title'>Contact information:</h4>
+	      </div>
+	      <div class="modal-body">
+	         <video  src="http://oky.16mb.com/MYpublic/videoplayback2.ogg" type="video/ogg"  autoplay loop></video>
+	         <div class="carousel-caption">
+	            <h3>idline: @153jkl</h3>
+	            <p>Email: cosmics@gmail.com</p>
+	        </div>
+	      </div>
+	      <div class="modal-footer">
+	        <button class="btn btn-success " data-dismiss="modal">Close</button>
+	  	  </div>
+	    </div>
+	  </div>
+	</div>
+	
+<!--Footer-->
+	<div class="footer" id="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 col-md-4">
+					<h4><b>Contact Us</b></h4>
+					<p><i class="fa fa-home" aria-hidden="true"></i> jalan inderalaya, Sumatera Selatan, Indonesia</p>
+					<p><i class="fa fa-envelope-o" aria-hidden="true"></i> cosmic@gmail.com</p>
+					<p><i class="fa fa-phone" aria-hidden="true"></i> 0711-30132</p>
+					<p><i class="fa fa-globe" aria-hidden="true"></i> www.cosmic.com</p>
+				</div>
+				<div class="col-lg-4 col-md-4">
+					<h4><b>About</b></h4>
+					<p><i class="fa fa-square-o" aria-hidden="true"></i> About Us</p>
+					<p><i class="fa fa-square-o" aria-hidden="true"></i> Privacy</p>
+					<p><i class="fa fa-square-o" aria-hidden="true"></i> Term & Condition</p>
+				</div>
+				<div class="col-lg-4 col-md-4">
+					<h4><b>Keep In Touch</b></h4>
+					<i class="social fa fa-facebook-official" aria-hidden="true"></i>
+					<i class="social fa fa-twitter" aria-hidden="true"></i>
+					<i class="social fa fa-instagram" aria-hidden="true"></i>
+					<i class="social fa fa-youtube-square" aria-hidden="true"></i>
+					<i class="social fa fa-linkedin" aria-hidden="true"></i>
+					<i class="social fa fa-pinterest-square" aria-hidden="true"></i>
+					<i class="social fa fa-github" aria-hidden="true"></i><br>
+					<input type="email" placeholder=" Subscribe For Updates" name="email"><button class="btn btn-success">SUBSCRIBE</button>
+				</div>	
+			</div>
+		</div>
+	</div>
+	
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/wow.min.js"></script>
+              <script>
+              new WOW().init();
+              </script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
